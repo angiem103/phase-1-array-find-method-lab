b8deb332e0b795cbf2dee6d060c5b5be9be88598
Angie <amaticorena103@hotmail.com>
2021-11-12 20:52:17 -0500

    commit tree b4c5dd0af60ea3e5a8a62d99ec64156dbde529a6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+function superbowlWin(record) {
+    let winningGame = record.find(record => record.result ==='W')
+    console.log(!!winningGame)
+   if(winningGame ===false ){return winningGame.year}
+else {return undefined}
+  }
